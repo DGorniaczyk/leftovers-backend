d7cf80c08629563024d06dca3258736a97aa8e65
DGorniaczyk <dawidoo12345678@gmail.com>
2026-06-18 09:58:45 +0200

    Fix: Hashed password for seed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'User signup' })
+  @ApiResponse({ status: 201, description: 'User successfully signed up' })
+  @ApiResponse({ status: 400, description: 'User already exists' })
   @Post('signup')
   async signup(
     @Body() { email, password }: { email: string; password: string },
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
